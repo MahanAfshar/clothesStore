b9afafa8c31ccf86fa563fb00184e73359e18b3d
MahanAfshar <Mahan.Ghaffarzadeh.Afshar@gmail.com>
2024-09-15 22:28:58 +0330

    update README.md

---
+++ README.md
@@ -0,0 +1,32 @@
+# Demo
+
+[![demo](./style/gifs/demo.gif)](#)
+
+## Clothes store
+
+### **[Review online](#)**
+
+## Description
+
+lorem ipsum
+
+## Features
+
+* Fully responsive for mobile, tablet, laptop.
+* Beautiful user interface.
+* Mobile first.
+* Login and sign up.
+
+## Technologies
+
+* Javascript
+* Css
+* Html
+
+## Contact me
+
+### **[Email](mailto:mahan.ghaffarzadeh.afshar@gmail.com)**
+
+### **[Linkedin](https://ir.linkedin.com/in/mahan-afshar-308aa2321?trk=people-guest_people_search-card)**
+
+### **[Telegram](https://t.me/mahanafshaar)**
--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+const hamburgerButton = document.getElementById('hamburgerButton').classList;
+const navMenu = document.getElementById('navMenu').classList;
 const coursel = document.querySelector('.comment');
 const arrowButtons = document.querySelectorAll('.arrow');
-let isDragging = false, startX, stratScrollLeft;
-const firstCardWidth = coursel.querySelector('.commentCard').offsetWidth;
+const firstCardWidth = document.querySelector('.commentCard').offsetWidth;
 const registerButton = document.querySelector('#registerButton');
 const form = document.querySelector('.wrapperForm');
 const closeButton = document.querySelector('span.fa-close');
@@ -12,159 +13,123 @@ const registerForm = document.querySelector('.registerForm');
 const loginForm = document.querySelector('.loginForm');
 const textInput = document.querySelectorAll('.textInput');
 const mobileInput = document.querySelectorAll('.mobileInput');
+const passwordSection = document.querySelectorAll('.passwordSection');
 const passwordInput = document.querySelectorAll('.passwordInput');
 const suggest = document.querySelector('.suggest');
 const closeButton2 = document.querySelector('.closeButton2');
 const toggleTheme = document.querySelectorAll('.toggleColor');
+let isDragging = false, startX, stratScrollLeft;
 
-function toggleMenu()
-{
-    const hamburgerButton = document.getElementById('hamburgerButton').classList;
-    const navMenu = document.getElementById('navMenu').classList;
-    if(hamburgerButton.contains('active'))
-    {
-        hamburgerButton.remove('active');
-        navMenu.remove('active');
-    }
-    else
-    {
-        hamburgerButton.add('active');
-        navMenu.add('active');
-    }
-}
+function toggleMenu() {
+    hamburgerButton.toggle('active');
+    navMenu.toggle('active');
+};
 
 arrowButtons.forEach(btn => {
-    btn.addEventListener('click', function() {
+    btn.addEventListener('click', () => {
         coursel.scrollLeft += btn.classList.contains('left') ? -firstCardWidth : firstCardWidth;
-    })
-})
+    });
+});
 
-function dragStart(e)
-{
+const dragStart = (e) => {
     isDragging = true;
     coursel.classList.add('dragging');
     startX = e.pageX;
     stratScrollLeft = coursel.scrollLeft;
-}
+};
 
-function dragging(e)
-{
+const dragging = (e) => {
     if (!isDragging) return;
     coursel.scrollLeft = stratScrollLeft - (e.pageX - startX);
-}
+};
 
-function dragStop(e)
-{
+const dragStop = (e) => {
     isDragging = false;
     coursel.classList.remove('dragging');
-}
+};
 
 coursel.addEventListener('mousedown', dragStart);
 coursel.addEventListener('mousemove', dragging);
 document.addEventListener('mouseup', dragStop);
 
-registerButton.addEventListener('click', ()=> {
-        form.classList.add('active');
-        let newDiv = document.createElement('div');
-        newDiv.style.width = '100%';
-        newDiv.style.height = '100%';
-        newDiv.style.position = 'fixed';
-        newDiv.style.top = '0';
-        newDiv.style.left = '0';
-        newDiv.style.backdropFilter = 'blur(16px)';
-        newDiv.style.backgroundColor = 'rgba(0, 0, 0, .25)';
-        document.body.appendChild(newDiv);
-})
-
-closeButton.addEventListener('click', ()=> {
-        form.classList.remove('active');
-        document.body.removeChild(document.body.lastElementChild)
-})
-
-login.addEventListener('click', ()=> {
-    registerForm.style.right = '1000%';
-    loginForm.style.left = '0';
-    wrapperForm.style.height = '350px';
+registerButton.addEventListener('click', () => {
+    form.classList.add('active');
+    registerForm.classList.add('active');
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('blurPage');
+    document.body.appendChild(newDiv);
 });
 
-register.addEventListener('click', ()=> {
-    loginForm.style.left = '1000%';
-    registerForm.style.right = '0';
-    wrapperForm.style.height = '500px';
+closeButton.addEventListener('click', () => {
+    form.classList.remove('active');
+    registerForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    document.body.lastElementChild.remove();
 });
 
-for (let i=0;i<textInput.length;i++)
-    textInput[i].addEventListener('keypress', textValidation);
+login.addEventListener('click', () => {
+    registerForm.classList.remove('active');
+    loginForm.classList.add('active');
+});
 
-for (let i=0;i<mobileInput.length;i++)
-    mobileInput[i].addEventListener('keypress', numberValidation);
+register.addEventListener('click', () => {
+    loginForm.classList.remove('active');
+    registerForm.classList.add('active');
+});
 
-for (let i=0;i<passwordInput.length;i++)
-    passwordInput[i].addEventListener('keyup', passwordValidation);
-
-function textValidation(e)
-{
+const textValidation = (e) => {
     if (e.keyCode >= 65 && e.keyCode <= 90 || e.keyCode >= 97 && e.keyCode <= 122 || e.keyCode == 32)
         return;
     else
         e.preventDefault();
 }
 
-function numberValidation(e)
-{
+const numberValidation = (e) => {
     if (e.keyCode >= 48 && e.keyCode <= 57)
         return;
     else
         e.preventDefault();
 }
 
-function passwordValidation()
-{
+const passwordValidation = () => {
     const passwordValue = passwordInput[0].value;
     const rePasswordValue = passwordInput[1].value;
-    passwordInput[0].addEventListener('blur', function() {
+    passwordInput[0].addEventListener('blur', () => {
         passwordInput[1].focus();
-    })
-    passwordInput[1].addEventListener('blur', function() {
-        if (passwordValue != rePasswordValue)
-        {
-            for (let i=0;i<passwordInput.length;i++)
-            {
-                passwordInput[i].style.borderBottom = '1px solid red';
-                passwordInput[i].nextElementSibling.style.color = 'red';
-            }
+    });
+    passwordInput[1].addEventListener('blur', () => {
+        if (passwordValue != rePasswordValue) {
+            for (let i = 0; i < passwordInput.length; i++) {
+                passwordSection[i].classList.remove('success');
+                passwordSection[i].classList.add('error');
+            };
         }
-        else
-        {
-            for (let i=0;i<passwordInput.length;i++)
-            {
-                passwordInput[i].style.borderBottom = '1px solid green';
-                passwordInput[i].nextElementSibling.style.color = 'green';
-            }
-        }
-    })
-}
+        else {
+            for (let i = 0; i < passwordInput.length; i++) {
+                passwordSection[i].classList.remove('error');
+                passwordSection[i].classList.add('success');
+            };
+        };
+    });
+};
 
-setTimeout(function() {
-    suggest.classList.add('active')
-}, 5000)
+for (let i = 0; i < textInput.length; i++)
+    textInput[i].addEventListener('keypress', textValidation);
 
-setTimeout(function() {
-    suggest.classList.remove('active')
-}, 15000)
+for (let i = 0; i < mobileInput.length; i++)
+    mobileInput[i].addEventListener('keypress', numberValidation);
+
+for (let i = 0; i < passwordInput.length; i++)
+    passwordInput[i].addEventListener('keyup', passwordValidation);
+
+setTimeout(function () {
+    suggest.classList.add('active');
+}, 5000);
+
+setTimeout(function () {
+    suggest.classList.remove('active');
+}, 15000);
 
 closeButton2.addEventListener('click', () => {
-    suggest.style.display = 'none';
-})
-
-toggleTheme.forEach((item, index) => {
-    item.addEventListener('click', function() {
-        if (index == 0)
-            document.body.style.backgroundColor = 'burlywood';
-        else if (index == 1)
-        
-            document.body.style.backgroundColor = 'white';
-        else
-            document.body.style.backgroundColor = 'darkgray';
-    })
-})
+    suggest.classList.remove('active');
+});
